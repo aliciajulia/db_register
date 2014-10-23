fcd32d515b426c712f5e50f8d2c0c73ffbd40d4f
alibro916 <alibro916@E29784.edu.linkoping.se>
2014-10-23 14:16:09 +0200

    klar

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ define("DB_NAME", "lekpark");
 $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
 
 //var_dump($_GET["svar"]);
-
+//<!--lägg till attraktion formulär-->
 if (!isset($_GET["svar"])) {
-    echo "det funkade inte";
+    
 } else {
     $svar = $_GET["svar"];
     $sql = "INSERT INTO attraktioner(id, namn) VALUES ('','$svar')";
@@ -26,15 +26,20 @@ if (!isset($_GET["svar"])) {
 
     foreach ($leksaker as $leksak) {
         echo "<br>";
+        echo $leksak["id"];
+        echo " ";
         echo $leksak["namn"];
         echo "<br>";
     }
 }
 
-if (!isset($_GET["id2"])){
+
+//ändra attraktionen
+if (!isset($_GET["id2"])) {
+    
 } else {
-     $id2 = $_GET["id2"];
-     $svar2 = $_GET["svar2"];
+    $id2 = $_GET["id2"];
+    $svar2 = $_GET["namn2"];
     $sql = "UPDATE attraktioner SET namn='$svar2' WHERE id=$id2";
 
     $stmt = $dbh->prepare($sql);
@@ -49,15 +54,21 @@ if (!isset($_GET["id2"])){
 
     foreach ($leksaker as $leksak) {
         echo "<br>";
+        echo $leksak["id"];
+        echo " ";
         echo $leksak["namn"];
         echo "<br>";
     }
 }
 
-if (!isset($_GET["id3"])){
+
+//ta bort en attraktion
+if (!isset($_GET["id3"])) {
+    
 } else {
-     $id3 = $_GET["id3"];
+    $id3 = $_GET["id3"];
     $sql = "DELETE FROM attraktioner WHERE id=$id3";
+
 
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
@@ -71,6 +82,8 @@ if (!isset($_GET["id3"])){
 
     foreach ($leksaker as $leksak) {
         echo "<br>";
+        echo $leksak["id"];
+        echo " ";
         echo $leksak["namn"];
         echo "<br>";
     }
@@ -83,7 +96,7 @@ if (!isset($_GET["id3"])){
     </head>
     <body>
         <!--        <a href="?kill">kill it</a>-->
-        
+
         <!--lägg till attraktion formulär-->
         <div class="attraktioner">
             Lägg till attraktion
@@ -92,27 +105,52 @@ if (!isset($_GET["id3"])){
                 <input type="submit" value="Lägg till"> 
                 <input type="hidden" value="Attraktion">
             </form>
-            
-            
+
+
             <!--ändra attraktion formulär-->
             <div class="attraktioner">
                 Ändra attraktion
                 <form>
                     ID PÅ DET DU VILL ÄNDRA = <input type="number" name="id2">
-                    NAMN = <input type="text" name="namn2">
+                    NYTT NAMN = <input type="text" name="namn2">
                     <input type="submit" value="Ändra"> 
                     <input type="hidden" value="Attraktion">
                 </form>
-                
-                
+
+
                 <!--ta bort attraktion formulär-->
                 <div class="attraktioner">
                     Ta bort attraktion
-                    <form>
-                        ID =  <input type="number" name="id3">
-                        <input type="submit" value="Ta bort"> 
-                        <input type="hidden" value="Attraktion">
-                    </form>
+                    <br>
+                    VÄLJ ATTRAKTION 
+                    <select>
+                        <!--                        <option value="volvo">-->
+                        <?php
+                        $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
+
+                        $stmt = $dbh->prepare($sql);
+                        $stmt->execute();
+
+                        $sql = "SELECT * FROM attraktioner";
+                        $stmt = $dbh->prepare($sql);
+                        $stmt->execute();
+
+                        $leksak = $stmt->fetchAll();
+
+
+                        foreach ($leksak as $namn) {
+                            echo "<option value=".$namn["id"].">".$namn["namn"]."</option>";
+                            echo "<br>";
+                        }
+                        ?>
+                        <!--                        </option>-->
+                    </select>
+                    <!--                    <form>
+                                            <
+                                            ID =  <input type="number" name="id3">
+                                            <input type="submit" value="Ta bort"> 
+                                            <input type="hidden" value="Attraktion">
+                                        </form>-->
                 </div>
                 </body>
                 </html>
